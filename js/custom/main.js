cb3620876d0692a34ed72ec471139afb93c5eb8a
Som webster <somnathfreak007@gmail.com>
2017-12-24 12:49:41 +0000

    Custom 3d model Trial

--- js/custom/main.js
+++ js/custom/main.js
@@ -1,8 +1,8 @@
 function main(){
     // var sprite = renderSprite(smoothedRoot);
 	// renderText("Full description \nof the item", sprite);    
-    renderBox(smoothedRoot);
-    // renderObject(smoothedRoot, "data/models/model.json");
+    //renderBox(smoothedRoot);
+     renderObject(smoothedRoot, "data/models/mode(2).json");
     // renderObject(smoothedRoot, "data/models/scene.json");
     
 }
