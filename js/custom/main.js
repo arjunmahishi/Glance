e16ae3c2188fb302bf6358b1c0cfa37f6c3e1456
Som webster <somnathfreak007@gmail.com>
2017-12-24 15:02:35 +0000

    Update main.js

--- js/custom/main.js
+++ js/custom/main.js
@@ -2,7 +2,7 @@ function main(){
     // var sprite = renderSprite(smoothedRoot);
 	// renderText("Full description \nof the item", sprite);    
     //renderBox(smoothedRoot);
-     renderObject(smoothedRoot, "data/models/model4.json");
+     renderObject(smoothedRoot, "data/models/model.json");
     // renderObject(smoothedRoot, "data/models/scene.json");
     
 }
